5105798fc29c56e6354416c0e5753e0995e46cef
Karyl <44136068+KarylReyne@users.noreply.github.com>
2023-12-06 17:15:50 +0100

    init branch

---
+++ src/executable.py
@@ -0,0 +1,5 @@
+from helpers.download import download_genesis_table
+
+
+if __name__ == "__main__":
+    download_genesis_table("https://www-genesis.destatis.de/genesis/online?sequenz=tabelleErgebnis&selectionname=21111-0014")
---
+++ src/helpers/download.py
@@ -0,0 +1,7 @@
+import pystatis as pyst
+
+
+def download_genesis_table(url: str):
+    # rqst_data = requests.get(url)
+    # rqst_data = json.loads(rqst_data.text)
+    # print(rqst_data)
