c6e6a29a4b9f0ec4570f05585a6838cbc1904114
Tom Freudenmann <tom.freudenmann@outlook.de>
2024-01-13 15:03:42 +0100

    Added abs rel plot and most common value

--- src/school_analysis/analysis/aggregation.py
+++ src/school_analysis/analysis/aggregation.py
@@ -4,7 +4,7 @@ import numpy as np
 class Aggregation:
 
     @staticmethod
-    def min_mean_max_grouping(df: pd.DataFrame, group: str = None, grouping_cols: list[str] = ["Year", "Type"], agg_col: str = "Percentage", group_col: str = "Federal State") -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    def min_mean_max_grouping(df: pd.DataFrame, group: str = None, grouping_cols: list[str] = ["Year", "Type"], agg_col: str = "Percentage", group_col: str = "Federal State", absolute: bool = True) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         """Aggregate the data by grouping_cols and calculate the min, mean and max of agg_col. This function uses first
         a prefiltering of the dataframe by group_col and group. If group is None, the dataframe will not be filtered.
         
@@ -23,8 +23,31 @@ class Aggregation:
         g = df[df[group_col].isin(group)] if group is not None else df.copy()
         
         # Aggregate the data
-        group_min = g.groupby(grouping_cols).apply(lambda x: x[agg_col].min()).reset_index().rename(columns={0: agg_col})
-        group_agg = g.groupby(grouping_cols).apply(lambda x: x[agg_col].mean()).reset_index().rename(columns={0: agg_col})
-        group_max = g.groupby(grouping_cols).apply(lambda x: x[agg_col].max()).reset_index().rename(columns={0: agg_col})
+        if absolute:
+            group_min = g.groupby(grouping_cols).apply(lambda x: x[agg_col].min()).reset_index().rename(columns={0: agg_col})
+            group_agg = g.groupby(grouping_cols).apply(lambda x: x[agg_col].mean()).reset_index().rename(columns={0: agg_col})
+            group_max = g.groupby(grouping_cols).apply(lambda x: x[agg_col].max()).reset_index().rename(columns={0: agg_col})
+        else:
+            group_avg = g.groupby(grouping_cols).apply(lambda x: x[agg_col].mean()).reset_index().rename(columns={0: agg_col})[agg_col].mean()
+            group_min = g.groupby(grouping_cols).apply(lambda x: 100 * (x[agg_col].min() / group_avg)).reset_index().rename(columns={0: agg_col})
+            group_agg = g.groupby(grouping_cols).apply(lambda x: 100 * (x[agg_col].mean() / group_avg)).reset_index().rename(columns={0: agg_col})
+            group_max = g.groupby(grouping_cols).apply(lambda x: 100 * (x[agg_col].max() / group_avg)).reset_index().rename(columns={0: agg_col})
+            
         
         return group_min, group_agg, group_max
+    
+    @staticmethod
+    def get_most_common_value(df: pd.DataFrame, target_col: str, group_cols: list[str]) -> pd.DataFrame:
+        """Get the most common value of target_col for each group in group_col.
+
+        Args:
+            df (pd.DataFrame): Dataframe to use.
+            target_col (str): Column to get the most common value from.
+            group_col (str): Column to use for grouping.
+
+        Returns:
+            pd.DataFrame: Dataframe with the most common value of target_col for each group in group_col.
+        """
+        most_common = df.groupby(group_cols).apply(lambda x: x[target_col].sum()).reset_index().rename(columns={0: target_col})
+        most_common = most_common.sort_values(target_col, ascending=False)
+        return most_common.reset_index(drop=True)
---
+++ src/school_analysis/plotting/general.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+class GeneralPlots:
+    
+    @staticmethod
+    def absolute_relative_plot(df: pd.DataFrame, x_column: str, y_column: str, label_column: str, label_types: list[str], grouping: list[str] = ["Year", "Gender"]) -> tuple[plt.Figure, plt.Axes]:
+        """Generates two plots. The first plot shows the absolute values of the y_column for the label_types. The second plot shows the relative values of the y_column for the label_types.
+
+        Args:
+            df (pd.DataFrame): Dataframe to plot.
+            x_column (str): Column to plot on the x-axis.
+            y_column (str): Column to plot on the y-axis.
+            label_column (str): Column to use for the legend and the grouping.
+            label_types (list[str]): List of all types to plot.
+            grouping (list[str], optional): Grouping for aggregating the data. Defaults to ["Year", "Gender"].
+
+        Returns:
+            tuple[plt.Figure, plt.Axes]: Tuple of the figure and the axes.
+        """
+        
+        # Plot the data
+        fig, axs = plt.subplots(1, 2, sharex=True)
+
+        # Get the number of students for the most common school label_types
+        plot_data = df[df[label_column].isin(label_types)]
+        plot_data = df.groupby(grouping + [label_column]).apply(lambda x: x[y_column].sum()).reset_index().rename(columns={0: y_column})
+        plot_data["Relative Value"] = plot_data.groupby(grouping)[y_column].transform(lambda x: x / x.sum())
+        # Plot the data
+        ax = axs[0]
+
+        for ct in label_types:
+            plot_data_st = plot_data[plot_data[label_column] == ct]
+            ax.plot(plot_data_st[x_column], plot_data_st[y_column], label=ct)
+            
+        # Other settings
+        ax.set_xlabel(x_column)
+        ax.set_ylabel(y_column)
+        ax.set_title("Absolute {}".format(y_column))
+        ax.grid(True)
+
+        ax = axs[1]
+        for ct in label_types:
+            plot_data_st = plot_data[plot_data[label_column] == ct]
+            ax.plot(plot_data_st[x_column], plot_data_st["Relative Value"], label=ct)
+        # Other settings
+        ax.set_xlabel(x_column)
+        ax.set_ylabel("Percents")
+        # ax.set_xticks(np.arange(plot_data["Year"].min(), plot_data["Year"].max() + 1, 3))
+        ax.set_title("Relative {}".format(y_column))
+        ax.grid(True)
+
+        # Build legend
+        handles, labels = ax.get_legend_handles_labels()
+        fig.legend(handles, labels, loc='right', bbox_to_anchor=(1.4, 0.5), title=label_column)
+
+        return fig, axs
